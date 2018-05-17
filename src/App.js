a4554e5595f6a27aab02131dcc77fc6be8080adf
Hiram Cruz <hiram0101@gmail.com>
2018-05-17 12:10:44 -0400

    updated app to be stateful

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import TopBar from "./components/TopBar"
 import Title from "./components/Title"
 import Game from "./components/Game"
 
-export default function App(props) {
-  return (
-    <main className="App">
-      <TopBar />
-      <Title />
-      <Game />
-    </main>
-  )
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      num: 0,
+      inputs: []
+    }
+  }
+
+  render() {
+    return (
+      <main className="App">
+        <TopBar />
+        <Title />
+        <Game />
+      </main>
+    )
+  }
 }
